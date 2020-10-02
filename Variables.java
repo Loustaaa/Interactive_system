99632db5b3fda5e2c8f392019e2dbd8f22cb6ba1
Jordan Barnes <jordan.barnes@newcastle.ac.uk>
2020-10-02 13:58:36 +0100

    2.3 solutions

--- Variables.java
+++ Variables.java
@@ -2,29 +2,26 @@ public class Variables {
 
     public static void main(String[] args) {
 
-        binaryToDecimal();
-        binaryToDecimal();
-        binaryToHex();
+
+
+        binaryToDecimal(); //ignore
+        binaryToDecimal(); //ignore
+        binaryToHex(); //ignore
 
     }
 
 
     public static void binaryToDecimal() {
 
-        String binary = "1010";
-        System.out.println(Integer.parseInt(binary, 2));
+
 
     }
 
     public static void binaryToOctal() {
-        String binary = "1010";
-        System.out.println(Integer.toOctalString(Integer.parseInt(binary,
-                2)));
+
     }
 
     public static void binaryToHex() {
-        String binary = "1010";
-        System.out.println(Integer.toHexString(Integer.parseInt(binary,
-                2)));
+
     }
 }
