26e48896125172b5beea0de344ceeea375dbf0a2
louiefranchino <louiefranchino@hotmail.com>
2020-10-21 11:09:16 +0100

    Added a few methods to the program

--- Circle.java
+++ Circle.java
@@ -1,6 +1,11 @@
 public class Circle {
 
     public static void main(String[] args) {
+        double radius = 7.5;
+        double perimeter = 2 * Math.PI * radius;
+        double area = Math.PI * (radius * radius);
 
+        System.out.println(perimeter);
+        System.out.println(area);
     }
 }
