df4b91f6e2f167e71d1f3012df15fa1da20ec19b
louiefranchino <louiefranchino@hotmail.com>
2020-11-27 19:04:58 +0000

    First version with complete SortedArrayList, User, book classes

--- Triangle.java
+++
@@ -1,9 +0,0 @@
-public class Triangle {
-
-    public static void main(String[] args) {
-        double width = 5.6;
-        double height = 8.5;
-        double area = (width * height) / 2;
-        System.out.println(area);
-    }
-}
